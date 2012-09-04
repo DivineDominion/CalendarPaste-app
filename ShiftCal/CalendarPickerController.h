00986185188ca8f4253da763517fd12923921e6c
Christian Tietze <christian.tietze@googlemail.com>
2012-09-04 19:06:20 +0200

    adds CalendarPickerController, groups view controllers in "Add Shift" group

---
+++ ShiftCal/CalendarPickerController.h
@@ -0,0 +1,13 @@
+//
+//  CalendarPickerController.h
+//  ShiftCal
+//
+//  Created by Christian Tietze on 04.09.12.
+//  Copyright (c) 2012 Christian Tietze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalendarPickerController : UITableViewController
+
+@end
