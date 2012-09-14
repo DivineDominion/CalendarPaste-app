5ab9053b340eda8ac9f9f9907629d60d55095837
Christian Tietze <christian.tietze@googlemail.com>
2012-09-14 15:38:58 +0200

    adds empty Reminder view controller

---
+++ ShiftCal/ReminderViewController.h
@@ -0,0 +1,13 @@
+//
+//  ReminderViewController.h
+//  ShiftCal
+//
+//  Created by Christian Tietze on 14.09.12.
+//  Copyright (c) 2012 Christian Tietze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReminderViewController : UITableViewController
+
+@end
---
+++ ShiftCal/ReminderViewController.m
@@ -0,0 +1,73 @@
+//
+//  ReminderViewController.m
+//  ShiftCal
+//
+//  Created by Christian Tietze on 14.09.12.
+//  Copyright (c) 2012 Christian Tietze. All rights reserved.
+//
+
+#import "ReminderViewController.h"
+
+@interface ReminderViewController ()
+
+@end
+
+@implementation ReminderViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+
+}
+
+@end
