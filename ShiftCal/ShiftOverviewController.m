41e04d4122db7027d0e37e16201565219052dc48
Christian Tietze <christian.tietze@googlemail.com>
2012-10-18 20:12:46 +0200

    displays complex duration label and shifts metadata in overview

--- ShiftCal/ShiftOverviewController.m
+++ ShiftCal/ShiftOverviewController.m
@@ -107,6 +107,196 @@
 }
 @end
 
+#pragma mark - ShiftOverviewCell
+
+#define CELL_HEIGHT 52.0f
+
+@interface DurationLabel : UIView
+{
+    UILabel *_hoursLabel;
+    UILabel *_hoursCaptionLabel;
+    UILabel *_minutesLabel;
+    UILabel *_minutesCaptionLabel;
+}
+
+@property (nonatomic, retain) UILabel *hoursLabel;
+@property (nonatomic, retain) UILabel *hoursCaptionLabel;
+@property (nonatomic, retain) UILabel *minutesLabel;
+@property (nonatomic, retain) UILabel *minutesCaptionLabel;
+
+- (void)setDurationHours:(NSUInteger)hours andMinutes:(NSUInteger)minutes;
+@end
+
+@implementation DurationLabel
+@synthesize hoursLabel = _hoursLabel;
+@synthesize hoursCaptionLabel = _hoursCaptionLabel;
+@synthesize minutesLabel = _minutesLabel;
+@synthesize minutesCaptionLabel = _minutesCaptionlabel;
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    
+    if (self)
+    {
+        float bottomMargin = frame.size.height - 2.0f;
+        
+        UIFont *durationFont   = [UIFont boldSystemFontOfSize:30.0];
+        UIColor *durationColor = [UIColor colorWithRed:128.0/256 green:151.0/256 blue:185.0/256 alpha:1.0];
+        
+        UIFont *labelFont   = [UIFont boldSystemFontOfSize:16.0];
+        UIColor *labelColor = [UIColor darkGrayColor];
+        
+        self.hoursLabel = [[[UILabel alloc] initWithFrame:CGRectMake(0.0, 5.0, 40.0, 30.0)] autorelease];
+        self.hoursLabel.backgroundColor = [UIColor clearColor];
+        self.hoursLabel.font = durationFont;
+        self.hoursLabel.textColor = durationColor;
+        self.hoursLabel.textAlignment = NSTextAlignmentCenter;
+        
+        self.hoursCaptionLabel = [[[UILabel alloc] initWithFrame:CGRectZero] autorelease];
+        self.hoursCaptionLabel.backgroundColor = [UIColor clearColor];
+        self.hoursCaptionLabel.font = labelFont;
+        self.hoursCaptionLabel.textColor = labelColor;
+        self.hoursCaptionLabel.textAlignment = NSTextAlignmentCenter;
+        self.hoursCaptionLabel.text = @"hrs";
+        [self.hoursCaptionLabel sizeToFit];
+        CGRect hoursCaptionFrame = CGRectMake(0.0, 0.0, 40.0, 0.0);
+        hoursCaptionFrame.size.height = self.hoursCaptionLabel.bounds.size.height;
+        hoursCaptionFrame.origin.y = bottomMargin - self.hoursCaptionLabel.bounds.size.height;
+        self.hoursCaptionLabel.frame = hoursCaptionFrame;
+        
+        self.minutesLabel = [[[UILabel alloc] initWithFrame:CGRectMake(40.0, 5.0, 40.0, 30.0)] autorelease];
+        self.minutesLabel.backgroundColor = [UIColor clearColor];
+        self.minutesLabel.font = durationFont;
+        self.minutesLabel.textColor = durationColor;
+        self.minutesLabel.textAlignment = NSTextAlignmentCenter;
+        
+        self.minutesCaptionLabel = [[[UILabel alloc] initWithFrame:CGRectZero] autorelease];
+        self.minutesCaptionLabel.backgroundColor = [UIColor clearColor];
+        self.minutesCaptionLabel.font = labelFont;
+        self.minutesCaptionLabel.textColor = labelColor;
+        self.minutesCaptionLabel.textAlignment = NSTextAlignmentCenter;
+        self.minutesCaptionLabel.text = @"min";
+        [self.minutesCaptionLabel sizeToFit];
+        CGRect minutesCaptionFrame = CGRectMake(40.0, 0.0, 40.0, 0.0);
+        minutesCaptionFrame.size.height = self.minutesCaptionLabel.bounds.size.height;
+        minutesCaptionFrame.origin.y = bottomMargin - self.minutesCaptionLabel.bounds.size.height;;
+        self.minutesCaptionLabel.frame = minutesCaptionFrame;
+        
+        [self addSubview:self.hoursLabel];
+        [self addSubview:self.hoursCaptionLabel];
+        [self addSubview:self.minutesLabel];
+        [self addSubview:self.minutesCaptionLabel];
+    }
+    
+    return self;
+}
+
+- (void)setDurationHours:(NSUInteger)hours andMinutes:(NSUInteger)minutes
+{
+    self.hoursLabel.text = [NSString stringWithFormat:@"%d", hours];
+    if (hours == 1)
+    {
+        self.hoursCaptionLabel.text = @"hr";
+    }
+    else
+    {
+        self.hoursCaptionLabel.text = @"hrs";
+    }
+    
+    self.minutesLabel.text = [NSString stringWithFormat:@"%02d", minutes];
+    self.minutesCaptionLabel.text = @"min";
+    
+    [self setNeedsLayout];
+}
+
+@end
+
+@interface ShiftOverviewCell : UITableViewCell
+{
+    ShiftTemplate *_shift;
+    DurationLabel *_durationLabel;
+    UILabel *_calendarLabel;
+}
+
+@property (nonatomic, retain) ShiftTemplate *shift;
+@property (nonatomic, retain) DurationLabel *durationLabel;
+@property (nonatomic, retain) UILabel *calendarLabel;
+
+- (id)initAndReuseIdentifier:(NSString *)cellIdentifier;
+@end
+
+@implementation ShiftOverviewCell
+
+@synthesize shift = _shift;
+@synthesize durationLabel = _durationLabel;
+@synthesize calendarLabel = _calendarLabel;
+
+- (id)initAndReuseIdentifier:(NSString *)cellIdentifier
+{
+    self = [super initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellIdentifier];
+    
+    if (self)
+    {
+        self.durationLabel = [[[DurationLabel alloc] initWithFrame:CGRectMake(0.0f, 0.0, 80.0f, CELL_HEIGHT)] autorelease];
+        
+        self.calendarLabel = [[[UILabel alloc] initWithFrame:CGRectZero] autorelease];
+        self.calendarLabel.textAlignment = NSTextAlignmentRight;
+        self.calendarLabel.font = [UIFont boldSystemFontOfSize:12.0];
+        self.calendarLabel.textColor = [UIColor colorWithRed:120.0/256 green:120.0/256 blue:170.0/256 alpha:1.0];
+        
+        [self.contentView addSubview:self.durationLabel];
+        [self.contentView addSubview:self.calendarLabel];
+    }
+    
+    return self;
+}
+
+- (void)dealloc
+{
+    [_durationLabel release];
+    [_calendarLabel release];
+    
+    [super dealloc];
+}
+
+- (void)setShift:(ShiftTemplate *)shift
+{
+    if (shift != _shift)
+    {
+        [_shift release];
+        _shift = [shift retain];
+        
+        self.textLabel.text = shift.title;
+        self.detailTextLabel.text = shift.location;
+        self.calendarLabel.text = shift.calendarTitle;
+        
+        [self.durationLabel setDurationHours:[shift.durHours integerValue] andMinutes:[shift.durMinutes integerValue]];
+    }
+}
+
+- (void)layoutSubviews {
+    static double kCellVisualHeight = CELL_HEIGHT - 1.0f;
+    static double kTextWidth        = 200.0f;
+    
+    [super layoutSubviews];
+    
+    CGRect textFrame = CGRectMake(90.0f, 0.0f, kTextWidth, kCellVisualHeight);
+    self.textLabel.backgroundColor = [UIColor clearColor];
+    self.textLabel.frame = textFrame;
+    
+    CGRect detailFrame = CGRectMake(90.0f, 34.0f, kTextWidth, 18.0f);
+    self.detailTextLabel.frame = detailFrame;
+    
+    //CGRect durationFrame = CGRectMake(0.0f, 0.0, 80.0f, CELL_HEIGHT);
+    //self.durationLabel.frame = durationFrame;
+    
+    CGRect calendarFrame = CGRectMake(100.0f, 0.0, 210.0f, 18.0f);
+    self.calendarLabel.frame = calendarFrame;
+}
+
+@end
+
 #pragma mark - ShiftOverviewController
 
 @interface ShiftOverviewController ()
@@ -212,20 +402,25 @@
     return [self.shiftCollection countOfShifts];
 }
 
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return CELL_HEIGHT;
+}
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     static NSString *kCellIdentifier = @"shiftcell";
     
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kCellIdentifier];
+    ShiftOverviewCell *cell = [tableView dequeueReusableCellWithIdentifier:kCellIdentifier];
     NSInteger row = [indexPath row];
     
     if (!cell)
     {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:kCellIdentifier];
+        cell = [[ShiftOverviewCell alloc] initAndReuseIdentifier:kCellIdentifier];
     }
     
     ShiftTemplate *shift = [self.shiftCollection shiftAtIndex:row];
-    cell.textLabel.text = shift.title;
+    cell.shift = shift;
     
     return cell;
 }
