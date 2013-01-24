597b44c69195ee6246c3600a4e2d4c46f0734a6d
Christian Tietze <christian.tietze@gmail.com>
2013-01-24 20:00:05 +0100

    displays all-day events as such in the overview

--- ShiftCal/ShiftOverviewCell.m
+++ ShiftCal/ShiftOverviewCell.m
@@ -10,6 +10,7 @@
 
 #define LABEL_WIDTH 40.0f
 #define SECOND_LABEL_X LABEL_WIDTH
+#define STD_LEFT_INDENT 5.0f
 #define TIME_LABEL_Y 5.0f
 #define TIME_LABEL_HEIGHT 30.0f
 #define CAPTION_LABEL_Y (TIME_LABEL_Y + TIME_LABEL_HEIGHT - 5.0f)
@@ -28,17 +29,23 @@ BOOL _enableTwoDigits = NO;
     UILabel *_hoursCaptionLabel;
     UILabel *_minutesLabel;
     UILabel *_minutesCaptionLabel;
+    
+    BOOL _allDay;
+    UILabel *_allDayLabel;
 }
 
 @property (nonatomic, retain) UILabel *hoursLabel;
 @property (nonatomic, retain) UILabel *hoursCaptionLabel;
 @property (nonatomic, retain) UILabel *minutesLabel;
 @property (nonatomic, retain) UILabel *minutesCaptionLabel;
+@property (nonatomic, assign) BOOL allDay;
+@property (nonatomic, retain) UILabel *allDayLabel;
 
 - (void)setDurationHours:(NSUInteger)hours andMinutes:(NSUInteger)minutes;
 
 + (UILabel *)timeLabelWithLeftIndent:(float)leftIndent;
 + (UILabel *)captionLabel:(NSString *)caption leftIndent:(float)leftIndent;
++ (UILabel *)allDayLabel;
 
 - (void)compact;
 - (void)expand;
@@ -52,6 +59,8 @@ BOOL _enableTwoDigits = NO;
 @synthesize hoursCaptionLabel = _hoursCaptionLabel;
 @synthesize minutesLabel = _minutesLabel;
 @synthesize minutesCaptionLabel = _minutesCaptionlabel;
+@synthesize allDay = _allDay;
+@synthesize allDayLabel = _allDayLabel;
 
 - (id)initWithFrame:(CGRect)frame
 {
@@ -59,12 +68,16 @@ BOOL _enableTwoDigits = NO;
     
     if (self)
     {
-        self.hoursLabel   = [DurationLabel timeLabelWithLeftIndent:5.0f];
+        self.allDayLabel = [DurationLabel allDayLabel];
+        self.allDayLabel.hidden = YES;
+        
+        self.hoursLabel   = [DurationLabel timeLabelWithLeftIndent:STD_LEFT_INDENT];
         self.minutesLabel = [DurationLabel timeLabelWithLeftIndent:SECOND_LABEL_X];
         
-        self.hoursCaptionLabel   = [DurationLabel captionLabel:@"hrs" leftIndent:5.0f];
+        self.hoursCaptionLabel   = [DurationLabel captionLabel:@"hrs" leftIndent:STD_LEFT_INDENT];
         self.minutesCaptionLabel = [DurationLabel captionLabel:@"min" leftIndent:SECOND_LABEL_X];
         
+        [self addSubview:self.allDayLabel];
         [self addSubview:self.hoursLabel];
         [self addSubview:self.hoursCaptionLabel];
         [self addSubview:self.minutesLabel];
@@ -72,6 +85,34 @@ BOOL _enableTwoDigits = NO;
     }
     
     return self;
+}
+
+- (void)dealloc
+{
+    [_hoursCaptionLabel release];
+    [_hoursLabel release];
+    [_minutesCaptionLabel release];
+    [_minutesLabel release];
+    [_allDayLabel release];
+    
+    [super dealloc];
+}
+
+- (void)setAllDay:(BOOL)allDay
+{
+    if (_allDay == allDay)
+    {
+        return;
+    }
+    
+    self.hoursLabel.hidden = allDay;
+    self.minutesLabel.hidden = allDay;
+    self.hoursCaptionLabel.hidden = allDay;
+    self.minutesCaptionLabel.hidden = allDay;
+    
+    self.allDayLabel.hidden = !allDay;
+    
+    _allDay = allDay;
 }
 
 - (void)setDurationHours:(NSUInteger)hours andMinutes:(NSUInteger)minutes
@@ -131,6 +172,34 @@ BOOL _enableTwoDigits = NO;
     label.highlightedTextColor = [UIColor whiteColor];
     
     label.text = caption;
+    
+    return [label autorelease];
+}
+
++ (UILabel *)allDayLabel
+{
+    CGRect frame = CGRectMake(0, 0, (STD_LEFT_INDENT + 2 * LABEL_WIDTH), (CAPTION_LABEL_Y + CAPTION_LABEL_HEIGHT));
+    
+    UILabel *label = [[UILabel alloc] initWithFrame:frame];
+
+    UILabel *timeLabel = [DurationLabel timeLabelWithLeftIndent:STD_LEFT_INDENT];
+    frame = timeLabel.frame;
+    frame.size.width = (2 * LABEL_WIDTH);
+    frame.size.height = frame.size.height + 3.0f;
+    timeLabel.frame = frame;
+    
+    UIFont *labelFont = [UIFont boldSystemFontOfSize:28.0];
+    timeLabel.font = labelFont;
+    
+    timeLabel.text = @"all";
+    
+    UILabel *captionLabel = [DurationLabel captionLabel:@"day" leftIndent:STD_LEFT_INDENT];
+    frame = captionLabel.frame;
+    frame.size.width = (2 * LABEL_WIDTH);
+    captionLabel.frame = frame;
+    
+    [label addSubview:timeLabel];
+    [label addSubview:captionLabel];
     
     return [label autorelease];
 }
@@ -276,6 +345,7 @@ BOOL _enableTwoDigits = NO;
     self.detailTextLabel.text = shift.location;
     self.calendarLabel.text = shift.calendarTitle;
     
+    self.durationLabel.allDay = [shift isAllDay];
     [self.durationLabel setDurationHours:[shift.durHours integerValue] andMinutes:[shift.durMinutes integerValue]];
 }
 
